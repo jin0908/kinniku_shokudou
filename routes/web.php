0fe2d07201dc09ac8eecdbf8e3495f7c37eea18b
makoto <blue.0908@outlook.jp>
2022-11-27 18:56:04 +0900

    ルーティング修正

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,7 @@ Route::get('/record', [App\Http\Controllers\RecordController::class, 'record'])-
 Route::get('/record_edit', [App\Http\Controllers\RecordController::class, 'input'])->name('record_edit');
 //記録一覧編集
 Route::post('/record_update/{id}', [App\Http\Controllers\RecordController::class, 'record_edit'])->name('record_update');
+//記録入力画面
+Route::get('/add', [App\Http\Controllers\RecordController::class, 'add'])->name('add');
 //記録入力
-Route::get('/record_add', [App\Http\Controllers\RecordController::class, 'record_add'])->name('record_add');
+Route::post('/record_add', [App\Http\Controllers\RecordController::class, 'record_add'])->name('record_add');
