aeed24411912588090174ef0b620c0b12300e343
makoto <blue.0908@outlook.jp>
2022-11-29 12:46:28 +0900

    DBへデータ追加コーディング

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -17,28 +17,52 @@ class RecordController extends Controller
     
     //記録入力画面を表示
     public function add(){
+        //ログインしているデータを取得
+        $user = \Auth::user();
+        
 
         
-        return view('add'); 
+        return view('add', compact('user')); 
+        
+
     }
     
     
     //記録入力
     public function record_add(Request $request){
-
         
-        Record::create([
-            'user_id' => Auth::user()->id,
-            'date'=>$request->date,
-            'weight'=> $request->weight,
-            'kcal'=> $request->kcal,            
-            'protein'=> $request->protein,
-            'fat'=> $request->fat,
-            'carb'=> $request->carb,
+        foreach($request["record"] as $value){
+            $record = new Record();
+            $record->date = $value['date'];
+            $record->user_id = $value['id'];
+            $record->weight = $value['weight'];
+            $record->kcal = $value['kcal'];
+            $record->protein = $value['protein'];
+            $record->fat = $value['fat'];
+            $record->carb = $value['carb'];
+            $record->save();
 
-        
-    ]);
-    return redirect()->route('record');      
+            }
+
+            
+
+
+
+        return redirect()->route('record');      
+        }
+
 }
 
-}
+/*$date = $request;
+$serialize_request = serialize($request);
+dd($serialize_request);
+
+
+    Record::create([
+    'user_id' => Auth::user()->id,
+    'date'=>$request->date,
+    'weight'=> $request->weight,
+    'kcal'=> $request->kcal,            
+    'protein'=> $request->protein,
+    'fat'=> $request->fat,
+    'carb'=> $request->carb,*/
