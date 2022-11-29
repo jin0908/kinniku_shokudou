3c53ed57d409be0dd643eef1d759c39516d52cc1
makoto <blue.0908@outlook.jp>
2022-11-29 12:45:16 +0900

    配列追加

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -12,23 +12,25 @@
                 </tr>
             </thead>    
                 <tr>
-                    <td>1<input type="hidden" value="2022-11-1" name="date"></td>
-                    <td><input type="number" value="50" step="0.1"  min="10" max="120" class="form-control"  name="weight"></td>
-                    <td><input type="number" value="0" step="1"  min="0" max="4000" class="form-control"  name="kcal"></td>
-                    <td><input type="number" value="0" step="1"  min="0" max="500" class="form-control"  name="protein"></td>
-                    <td><input type="number" value="0" step="1"  min="0" max="500" class="form-control"  name="fat" ></td>
-                    <td><input type="number" value="0" step="1"  min="0" max="1000" class="form-control"  name="carb"></td>
+                    <td>1<input type="hidden" value="2022-11-01" name="record[1]['date']"></td>
+                    <td><input type="hidden" value="{{ $user->id }}" name="record[1]['id']"></td>
+                    <td><input type="number" value="50" step="0.1"  min="10" max="120" class="form-control"  name="record[1]['weight']"></td>
+                    <td><input type="number" value="0" step="1"  min="0" max="4000" class="form-control"  name="record[1]['kcal']"></td>
+                    <td><input type="number" value="0" step="1"  min="0" max="500" class="form-control"  name="record[1]['protein']"></td>
+                    <td><input type="number" value="0" step="1"  min="0" max="500" class="form-control"  name="record[1][fat]" ></td>
+                    <td><input type="number" value="0" step="1"  min="0" max="1000" class="form-control"  name="record[1][carb]"></td>
                 </tr>
+    
                 <tr>
-                <tr>
-                    <td>2<input type="hidden" value="2022-11-2" name="date"></td>
-                    <td><input type="number" value="50" step="0.1"  min="10" max="120" class="form-control"  name="weight"></td>
-                    <td><input type="number" value="0" step="1"  min="0" max="4000" class="form-control"  name="kcal"></td>
-                    <td><input type="number" value="0" step="1"  min="0" max="500" class="form-control"  name="protein"></td>
-                    <td><input type="number" value="0" step="1"  min="0" max="500" class="form-control"  name="fat" ></td>
-                    <td><input type="number" value="0" step="1"  min="0" max="1000" class="form-control"  name="carb"></td>
+                    <td>2<input type="hidden" value="2022-11-02" name="record[2][date]"></td>
+                    <td><input type="hidden" value="{{ $user->id }}" name="record[2]['id']"></td>
+                    <td><input type="number" value="50" step="0.1"  min="10" max="120" class="form-control"  name="record[2]['weight']"></td>
+                    <td><input type="number" value="0" step="1"  min="0" max="4000" class="form-control"  name="record[2]['kcal']"></td>
+                    <td><input type="number" value="0" step="1"  min="0" max="500" class="form-control"  name="record[2]['protein']"></td>
+                    <td><input type="number" value="0" step="1"  min="0" max="500" class="form-control"  name="record[2]['fat']" ></td>
+                    <td><input type="number" value="0" step="1"  min="0" max="1000" class="form-control"  name="record[2]['carb']"></td>
                 </tr>
-                <tr>
+                
                 
         </table>
         <input type="submit" value="入力">
