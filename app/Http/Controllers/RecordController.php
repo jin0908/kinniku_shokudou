671bf8efb9c3bb15bbb2057ca97b8e273f245ac4
makoto <blue.0908@outlook.jp>
2022-11-30 12:16:14 +0900

    記録一覧画面の表示修正

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -5,14 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Record;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
 
 class RecordController extends Controller
 {
     //記録一覧画面を表示
     public function record(){
 
-        
-        return view('record'); 
+        //アカウントデータを取得
+        $user = \Auth::user();
+
+        //アカウントと紐づく記録一覧取得
+        $record = Record::where('user_id',$user['id'])->orderBy('date','asc')->get();
+        //dd($record);
+        return view('record', compact('user','record')); 
     }
     
     //記録入力画面を表示
@@ -30,39 +37,31 @@ class RecordController extends Controller
     
     //記録入力
     public function record_add(Request $request){
+
+        //アカウントデータを取得
+        $user = \Auth::user();
         
         foreach($request["record"] as $value){
             $record = new Record();
+            
+            $record->user_id =  $user['id'];
             $record->date = $value['date'];
-            $record->user_id = $value['id'];
             $record->weight = $value['weight'];
             $record->kcal = $value['kcal'];
             $record->protein = $value['protein'];
             $record->fat = $value['fat'];
             $record->carb = $value['carb'];
             $record->save();
-
             }
-
-            
-
-
 
         return redirect()->route('record');      
         }
 
+
+         //記録一覧編集画面を表示
+        public function record_edit($id){
+    
+        
+    }
 }
 
-/*$date = $request;
-$serialize_request = serialize($request);
-dd($serialize_request);
-
-
-    Record::create([
-    'user_id' => Auth::user()->id,
-    'date'=>$request->date,
-    'weight'=> $request->weight,
-    'kcal'=> $request->kcal,            
-    'protein'=> $request->protein,
-    'fat'=> $request->fat,
-    'carb'=> $request->carb,*/
